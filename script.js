28b7776756e3d52c019fc38ec9232e7e8eb26b04
Dragoș Mateo Dămoc <56765258+dragosmatteo@users.noreply.github.com>
2021-06-08 17:48:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 // !! Dragos Matteo 5.06.2021 Folio
 
-const navbar =  document.querySelector('.navbar');
-let sticky = navbar.getBoundingClientRect().height;
+// const navbar =  document.querySelector('.navbar');
+// let sticky = navbar.getBoundingClientRect().height;
 
+// // !! Sticky navigation
+// const navbarScroll = () => {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky')
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// };
 
-// !! Sticky navigation
-const navbarScroll = () => {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky')
-  } else {
-    navbar.classList.remove('sticky');
-  }
-};
+// window.onscroll = navbarScroll;
 
-window.onscroll = navbarScroll;
+// !! Modal 
+
+const modal = document.querySelector('#simpleModal');
+const closeBtn = document.querySelector('.closeBtn');
+const modalBtn = document.querySelector('#contact')
+
+// !!  Lisen for click 
+modalBtn.addEventListener('click', openModal());
+
+// !! Lisen for close click
+closeBtn.addEventListener('click', closesModal());
+
+// !! Lisen for outside click 
+window.addEventListener('click', clickOutsite());
+
+// !! Function to open modal 
+const openModal = function(){
+  modal.style.display = 'block';
+}
+
+// !! Function to close modal 
+const closeModal = function(){
+  modal.style.display = 'none';
+}
+
+const clickOutsite = function(e){
+    if (e.target == modal) 
+    modal.style.display = 'none';
+}
